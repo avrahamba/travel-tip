125b5480e950126697a8f45ab014754be7933bca
Vlad Indikt <vlad.indikt@gmail.com>
2020-02-19 15:27:40 +0200

    removed the consolelog form get locs

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -3,7 +3,7 @@ const KEY = 'AIzaSyCaQVlcIeYewnFSmm3xkL2d3HHy9xhYbz4';
 const gTestadress ='הבונים, 4 רמת גן'
 function getLocs(address) {
     return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?&address=${address}&key=${KEY}`)
-        .then(res => console.log(res.data.results[0].geometry.location))
+        .then(res => res.data.results[0].geometry.location)
 }
 
 
