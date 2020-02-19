8b8cb9d5a491803da8cabff6f9d4a53d049f205c
avraham <b.a.avraham@gmail.com>
2020-02-19 18:03:51 +0200

    style

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ import { weatherService } from './services/weather.service.js'
 window.onload = () => {
 
     getPositionFromUrl()
-    .then((pos)=>{
-        if(pos)return pos;
-        else return locService.getPosition()
-    })
+        .then((pos) => {
+            if (pos) return pos;
+            else return locService.getPosition()
+        })
         .then(pos => {
             return pos.coords;
         })
@@ -23,8 +23,9 @@ window.onload = () => {
             const lng = coords.longitude;
             return mapService.initMap(lat, lng);
         })
-        .then(map => {mapService.addMarker(map.center)
-        return map;
+        .then(map => {
+            mapService.addMarker(map.center)
+            return map;
         })
         .then(map => weatherService.getWeather(map.center))
         .then(renderWeather)
@@ -33,15 +34,30 @@ window.onload = () => {
 document.querySelector('.btn-go').addEventListener('click', (ev) => {
     const nameLocation = document.querySelector('.name-location').value;
     locService.getLocs(nameLocation)
-        .then(loc => {mapService.panTo(loc);
-        return loc
+        .then(loc => {
+            mapService.panTo(loc);
+            return loc
         })
-        .then(loc=>weatherService.getWeather(loc))
+        .then(loc => weatherService.getWeather(loc))
         .then(renderWeather)
 })
 
-document.querySelector('.btn-my-copy').addEventListener('click',(ev)=>{
+document.querySelector('.btn-my-copy').addEventListener('click', (ev) => {
     console.log('ev :', ev);
+})
+
+document.querySelector('.btn-my-location').addEventListener('click', (ev) => {
+    locService.getPosition()
+        .then(loc => {
+            const coord = {
+                lat: loc.coords.latitude,
+                lng: loc.coords.longitude
+            }
+            mapService.panTo(coord);
+            return coord
+        })
+        .then(loc => weatherService.getWeather(loc))
+        .then(renderWeather)
 })
 
 function getParameterByName(name, url) {
@@ -58,13 +74,13 @@ function getPositionFromUrl() {
     const lat = +getParameterByName('lat');
     const lng = +getParameterByName('lng');
     if (!lat || !lng) return Promise.resolve(null);
-    return Promise.resolve({coords:{latitude:lat,longitude:lng}})
+    return Promise.resolve({ coords: { latitude: lat, longitude: lng } })
 }
-function renderWeather(watherObj){
+function renderWeather(watherObj) {
     console.log(watherObj);
-    
-    document.querySelector('.weather').innerHTML = 
-    `
+
+    document.querySelector('.weather').innerHTML =
+        `
     <h2>Weather today</h2>
     <img src="https://openweathermap.org/img/wn/${watherObj.icon}@2x.png" alt="">
     <div class="weather-text">
