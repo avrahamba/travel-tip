825876f24ad7b6d2b26d59745512fe11aafcb272
Vlad Indikt <vlad.indikt@gmail.com>
2020-02-19 14:54:26 +0200

    my second push testing getlocs

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,9 +1,9 @@
 var locs = [{ lat: 11.22, lng: 22.11 }]
 const KEY = 'AIzaSyCaQVlcIeYewnFSmm3xkL2d3HHy9xhYbz4';
-
+const gTestadress ='הבונים, 4 רמת גן'
 function getLocs(address) {
     return axios.get(`maps.googleapis.com/maps/api/geocode/json?&address=${address}&key=${KEY}`)
-        .then(res => console.log(res))
+        .then(res => console.log(res.data.results[0].geometry.location))
 }
 
 
