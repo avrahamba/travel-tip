9b1c72922e9c3232a97cd777b55791eee2ae910a
Vlad Indikt <vlad.indikt@gmail.com>
2020-02-19 15:08:44 +0200

    fixed axios bug

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -2,7 +2,7 @@ var locs = [{ lat: 11.22, lng: 22.11 }]
 const KEY = 'AIzaSyCaQVlcIeYewnFSmm3xkL2d3HHy9xhYbz4';
 const gTestadress ='הבונים, 4 רמת גן'
 function getLocs(address) {
-    return axios.get(`maps.googleapis.com/maps/api/geocode/json?&address=${address}&key=${KEY}`)
+    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?&address=${address}&key=${KEY}`)
         .then(res => console.log(res.data.results[0].geometry.location))
 }
 
